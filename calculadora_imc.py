e53c719990d2b9f85c3c4faa7a6b5618ac3c2cc7
Cezar Azevedo <cezaraom88@hotmail.com>
2023-04-02 18:37:28 -0300

    meu primeiro commit

---
+++ calculadora_imc.py
@@ -0,0 +1,8 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+weight2 = float(weight)
+height2 = float(height)
+bmi = weight2 / height2 ** 2
+bmi = int(bmi)
+print(bmi)
